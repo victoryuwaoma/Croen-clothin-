bc1a4ecc2ead9e121283d52518f8728e226df3db
Victory Uwaoma <victoryuwaoma@gmail.com>
2022-08-03 22:13:43 +0100

    Add files via upload
    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React, { Suspense, useEffect, lazy } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Header from "./components/header/header.component";
+import { GlobalStyle } from "./global.styles";
+import { useSelector, useDispatch } from "react-redux";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { checkUserSession } from "./redux/user/user.actions";
+import Spinner from "./components/spinner/spinner.component";
+import ErrorBoundary from "./components/error-boundary/error-boundary.component";
+
+const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
+const ShopPage = lazy(() => import("./pages/shop/shop.component"));
+const SignInAndSignUpPage = lazy(() =>
+  import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component")
+);
+const CheckoutPage = lazy(() => import("./pages/checkout/checkout.component"));
+
+const App = () => {
+  const currentUser = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(checkUserSession());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            <ErrorBoundary>
+            
+              <Route exact path="/" component={HomePage} />
+              <Route path="/shop" component={ShopPage} />
+              <Route exact path="/checkout" component={CheckoutPage} />
+              <Route
+                exact
+                path="/signin"
+                render={() =>
+                  currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+                }
+              />
+            </ErrorBoundary>
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
